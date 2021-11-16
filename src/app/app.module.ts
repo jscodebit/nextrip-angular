5c8974a4eaa222008e8aa1cda3378cf76f3ff4db
jscodebit <soujanya.janapatla@gmail.com>
2021-11-16 10:37:27 -0500

    Created header, tab, tabs components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TabsComponent } from './components/tabs/tabs.component';
+import { TabComponent } from './components/tab/tab.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TabsComponent,
+    TabComponent,
   ],
   imports: [
     BrowserModule
