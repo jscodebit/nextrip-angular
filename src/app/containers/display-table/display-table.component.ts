c3861781e7b38708df3d2f5f4cde9a7534b05395
jscodebit <soujanya.janapatla@gmail.com>
2021-11-20 22:43:32 -0500

    Adjusting the stylings to make components mobile responsive

--- src/app/containers/display-table/display-table.component.ts
+++ src/app/containers/display-table/display-table.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
 import { HttpClientService } from 'src/app/services/http-services.service';
 
 @Component({
@@ -9,26 +7,15 @@ import { HttpClientService } from 'src/app/services/http-services.service';
   templateUrl: './display-table.component.html',
   styleUrls: ['./display-table.component.css']
 })
-export class DisplayTableComponent implements OnInit, OnDestroy {
+export class DisplayTableComponent implements OnInit {
   headElements = ["ROUTE", "DESTINATION", "DEPARTS"];
   stopInformation: any = null;
   stopID: string; route : string; direction: string; stop: string;
   tableDescription: string; tableStop_ID: string; tablefieldElements: [];
-  private ngUnSubscribe = new Subject();
+  //private ngUnSubscribe = new Subject();
 
   constructor(private activateRoute : ActivatedRoute,
         private httpClientService: HttpClientService,){
-  }
-
-  ngOnInit(): void {
-    // this.activateRoute.queryParams
-    // .pipe(takeUntil(this.ngUnSubscribe))
-    // .subscribe(params => {
-    //   this.route = params['route'];
-    //   this.direction = params['direction'];
-    //   this.stop = params['stop'];
-    //   this.stopID = params['stop_id'];
-    // });
     this.route = this.activateRoute.snapshot.paramMap.get("route");
     this.direction = this.activateRoute.snapshot.paramMap.get("direction");
     this.stop = this.activateRoute.snapshot.paramMap.get("stop");
@@ -41,38 +28,42 @@ export class DisplayTableComponent implements OnInit, OnDestroy {
     }
   }
 
-  private fetchNextripRouteInfo(route, direction, stop) {
+  ngOnInit(): void {
+    // this.activateRoute.queryParams
+    // .pipe(takeUntil(this.ngUnSubscribe))
+    // .subscribe(params => {
+    //   this.route = params['route'];
+    //   this.direction = params['direction'];
+    //   this.stop = params['stop'];
+    //   this.stopID = params['stop_id'];
+    // });
+  }
+
+  private fetchNextripRouteInfo(route: string, direction: string, stop: string) {
     this.httpClientService.getStopsInformation(route, direction, stop)
-    .pipe(takeUntil(this.ngUnSubscribe))
     .subscribe(response => {
       this.stopInformation = response;
       if (this.stopInformation) {
-        this.populateDataToTabe(this.stopInformation);
+        this.populateDataToTable(this.stopInformation);
       }
     });
   }
 
-  private fetchNextripInfoByStopID(stopID){
+  private fetchNextripInfoByStopID(stopID: string){
     this.httpClientService.getStopsInformationByStopID(this.stopID)
-    .pipe(takeUntil(this.ngUnSubscribe))
     .subscribe(response => {
       this.stopInformation = response;
       if (this.stopInformation) {
-        this.populateDataToTabe(this.stopInformation);
+        this.populateDataToTable(this.stopInformation);
       }
     });
   }
 
-  populateDataToTabe(stopInformation){
+  populateDataToTable(stopInformation: string){
     this.tableDescription = this.stopInformation['stops'][0].description;
     this.tableStop_ID = this.stopInformation['stops'][0].stop_id;
     this.tablefieldElements = this.stopInformation['departures'];
     //console.log(typeof this.tablefieldElements, this.tablefieldElements)
-  }
-
-  ngOnDestroy(): void{
-    this.ngUnSubscribe.next();
-    this.ngUnSubscribe.complete();
   }
 
 }
